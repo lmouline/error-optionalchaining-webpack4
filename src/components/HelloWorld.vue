7047a3a73934b59bbd57fd4a0c9aa9252df79150
Ludovic Mouline <ludovic.mouline@share-now.com>
2021-06-17 11:23:40 +0200

    Initial

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <h3>{{text}}</h3>
+  <input />
+</template>
+
+<script lang="ts">
+import useCurrencyInput, { CurrencyInputOptions } from "vue-currency-input";
+import { computed, defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    condition: { type: Object}
+  },
+  setup(props) {
+    const options: CurrencyInputOptions = {
+      currency: "EUR"
+    }
+
+    const { formattedValue, inputRef } = useCurrencyInput(options)
+
+    const text = computed(() => {
+      if (props.condition?.isOk) {
+        return "Lalala";
+      }
+      return "Bababab";
+    });
+
+
+    return { text, inputRef, formattedValue }
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
